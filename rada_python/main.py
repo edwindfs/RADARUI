5167cc31f0d7f7c0c1be3fbfd8929a80ae594b1c
edwindfs <edwin@xcaliber.us>
2026-09-12 10:16:18 -0400

    Delete rada_python directory

--- rada_python/main.py
+++
@@ -1,7 +0,0 @@
-"""Main entry point for RADA Radar client (mirrors npm start)."""
-
-import sys
-from rada_python.v1_ldr_client import main
-
-if __name__ == "__main__":
-    main()
--- rada_python/services/__init__.py
+++
@@ -1,4 +0,0 @@
-from .vector_nav_service import VectorNavService
-from .vector_nav_ws_service import VectorNavWsService
-
-__all__ = ["VectorNavService", "VectorNavWsService"]
